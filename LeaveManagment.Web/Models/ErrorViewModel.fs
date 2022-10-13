cc421c405aac969eee511ba6e8583e7b203190b0
user <user@WIN-NJUHAFVKCKA>
2022-10-13 16:24:44 +0500

    Add project files.

---
+++ LeaveManagment.Web/Models/ErrorViewModel.fs
@@ -0,0 +1,9 @@
+namespace LeaveManagment.Web.Models
+
+open System
+
+type ErrorViewModel =
+    { RequestId: string }
+
+    member this.ShowRequestId =
+        not (String.IsNullOrEmpty(this.RequestId))
